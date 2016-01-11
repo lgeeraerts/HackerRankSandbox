a48507c9c7c20b5c08eac9dbcb7e423c7f7aa2d1
lgeeraerts <lgeeraerts@gmail.com>
2016-01-11 12:07:20 +0100

    Init

---
+++ ConsoleSandbox/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Sample program illustrating input/output methods */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int N, K;
+        string NK = Console.ReadLine();
+        string[] NandK = NK.Split(new Char[] { ' ', '\t', '\n' });
+        N = Convert.ToInt32(NandK[0]);
+        K = Convert.ToInt32(NandK[1]);
+
+        int[] C = new int[N];
+
+        string numbers = Console.ReadLine();
+        string[] split = numbers.Split(new Char[] { ' ', '\t', '\n' });
+
+        int i = 0;
+
+        foreach (string s in split)
+        {
+            if (s.Trim() != "")
+            {
+                C[i++] = Convert.ToInt32(s);
+            }
+        }
+
+        Process(C, K);
+    }
+    
+    static void Process(int[] flowers, int numberOfBuddies)
+    {
+        var buddyPurchaseCounts = new int[numberOfBuddies];
+        var flowersList = flowers.ToList();
+
+        var totalCost = 0;
+
+        while (flowersList.Count > 0)
+        {
+            var costliestFlower = flowersList.Max();
+            var purchaseCount = AddOneToBuddyWithLeastPurchases(buddyPurchaseCounts);
+
+            totalCost += costliestFlower * purchaseCount;
+            flowersList.Remove(costliestFlower);
+        }
+
+        Console.WriteLine(totalCost);
+    }
+
+    static int AddOneToBuddyWithLeastPurchases(int[] buddiesPurchaseCount)
+    {
+        var index = 0;
+        var smallest = buddiesPurchaseCount[0];
+
+        for (var i = 1; i < buddiesPurchaseCount.Length; i++)
+        {
+            if (buddiesPurchaseCount[i] < smallest)
+            {
+                index = i;
+            }
+        }
+        
+        buddiesPurchaseCount[index] = buddiesPurchaseCount[index] + 1;
+
+        return buddiesPurchaseCount[index];
+    }
+
+}
---
+++ SnakesAndLadders/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var t = Convert.ToInt32(Console.ReadLine());
+
+        for (var a0 = 0; a0 < t; a0++)
+        {
+            var board = ConstructBoard();
+
+            var numberOfLadders = Convert.ToInt32(Console.ReadLine());
+            for (var l0 = 0; l0 < numberOfLadders; l0++)
+            {
+                var ladder = Console.ReadLine().Split(' ');
+                var ladderSource = Convert.ToInt32(ladder[0]);
+                var ladderTarget = Convert.ToInt32(ladder[1]);
+
+                board[ladderSource].JumpToPosition = ladderTarget;
+            }
+            var numberOfSnakes = Convert.ToInt32(Console.ReadLine());
+            for (var s0 = 0; s0 < numberOfSnakes; s0++)
+            {
+                var snake = Console.ReadLine().Split(' ');
+                var snakeSource = Convert.ToInt32(snake[0]);
+                var snakeTarget = Convert.ToInt32(snake[1]);
+
+                board[snakeSource].JumpToPosition = snakeTarget;
+                board[snakeSource].IsSnake = true;
+            }
+
+            ProcessBoard(board, 1);
+        }
+    }
+
+    static void ProcessBoard(BoardPosition[] board, int startPosition)
+    {
+        var position = startPosition;
+        
+        while (position < 100)
+        {
+            var currentPosition = board[position];
+            var nextPosition = board[position + 1];
+            var nextThrowsAndDice = currentPosition.ThrowsAndDice.Advance();
+
+            nextPosition.SetThrowsAndDice(nextThrowsAndDice);
+
+            if (currentPosition.JumpToPosition.HasValue)
+            {
+                var targetPosition = board[currentPosition.JumpToPosition.Value];
+                var isUpdated = targetPosition.SetThrowsAndDice(currentPosition.ThrowsAndDice.Reset());
+
+                if (currentPosition.IsSnake)
+                {
+                    if (isUpdated)
+                    {
+                        position = currentPosition.JumpToPosition.Value;
+                        continue;
+                    }
+                    else if(currentPosition.ThrowsAndDice.DiceEyes == 6)
+                    {
+                        var nextSetPosition = FindNextNoSnakeSetPosition(board, position + 1);
+                        if (nextSetPosition == -1)
+                        {
+                            if (board[100].ThrowsAndDice == null)
+                            {
+                                board[100].ThrowsAndDice = new ThrowsAndDice();
+                            }
+                            board[100].ThrowsAndDice.Throws = -1;
+                            break;
+                        }
+                        else
+                        {
+                            position = nextSetPosition;
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            position++;
+        }
+
+        Console.WriteLine(board[100].ThrowsAndDice.Throws);
+    }
+
+    private static int FindNextNoSnakeSetPosition(BoardPosition[] board, int position)
+    {
+        for(var i = position; i < board.Length; i++)
+        {
+            if (board[i].IsVisited && !board[i].IsSnake) return i;
+        }
+
+        return -1;
+    }
+    
+    private static BoardPosition[] ConstructBoard()
+    {
+        var board = new BoardPosition[101];
+
+        for(var i = 1; i < board.Length; i++)
+        {
+            board[i] = new BoardPosition();
+        }
+
+        board[1].ThrowsAndDice = new ThrowsAndDice();
+        board[1].IsVisited = true;
+
+        return board;
+    }
+
+    private class BoardPosition
+    {
+        public bool IsVisited = false;
+        public ThrowsAndDice ThrowsAndDice;
+        public int? JumpToPosition;
+        public bool IsSnake = false;
+
+        public bool SetThrowsAndDice(ThrowsAndDice newThrowsAndDice) {
+            if (!IsVisited)
+            {
+                ThrowsAndDice = newThrowsAndDice;
+                IsVisited = true;
+                return true;
+            }
+            else
+            {
+                return ThrowsAndDice.UpdateIfBetter(newThrowsAndDice);
+            }
+        }
+    }
+
+    private class ThrowsAndDice
+    {
+        public int Throws = 0;
+        public int DiceEyes = 0;
+
+        public ThrowsAndDice Advance()
+        {
+            var newThrows = Throws;
+            if (DiceEyes == 0) newThrows++;
+            var newDiceEyes = DiceEyes + 1;
+            if(newDiceEyes == 7)
+            {
+                newDiceEyes = 1;
+                newThrows++;
+            }
+
+            return new ThrowsAndDice() { Throws = newThrows, DiceEyes = newDiceEyes };
+        }
+
+        public ThrowsAndDice Reset() {
+            return new ThrowsAndDice() { Throws = Throws, DiceEyes = 0 };
+        }
+
+        public bool UpdateIfBetter(ThrowsAndDice otherThrowsAndDice)
+        {
+            var thisThrows = this.DiceEyes == 0 ? this.Throws + 1 : this.Throws;
+            var otherThrows = otherThrowsAndDice.DiceEyes == 0 ? otherThrowsAndDice.Throws + 1 : otherThrowsAndDice.Throws;
+            
+            if (thisThrows > otherThrows)
+            {
+                this.Throws = otherThrowsAndDice.Throws;
+                this.DiceEyes = otherThrowsAndDice.DiceEyes;
+                return true;
+            }
+            else if (thisThrows == otherThrows && this.DiceEyes > otherThrowsAndDice.DiceEyes && otherThrowsAndDice.DiceEyes > 0)
+            {
+                this.DiceEyes = otherThrowsAndDice.DiceEyes;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
